24e6ba661dff081bb8d2ab65d5b0bef41be662cb
Snow <snow-official@outlook.com>
2024-12-10 15:54:22 +0800

    checkin

--- src/main/java/com/code/checkin/helper/job/CheckinJob.java
+++ src/main/java/com/code/checkin/helper/job/CheckinJob.java
@@ -17,45 +17,34 @@
 
 package com.code.checkin.helper.job;
 
-import cn.hutool.core.date.DatePattern;
-import cn.hutool.core.util.RandomUtil;
-import com.code.checkin.helper.service.CheckinActuator;
-import jakarta.annotation.Resource;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.TaskScheduler;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 /**
  * @author Snow
  * @date 2023/8/27 21:26
  */
 @Slf4j
-@Component
+// @Component
 public class CheckinJob {
 
-    @Resource
-    private CheckinActuator checkinActuator;
-
-    @Resource
-    private TaskScheduler taskScheduler;
-
-    @SneakyThrows
-    // @Scheduled(cron = "0 30 7 * * ?", zone = "Asia/Shanghai")
-    public void checkin() {
-        ZonedDateTime currentTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
-        ZonedDateTime latestTime = currentTime.plusHours(1);
-
-        long randomMax = latestTime.toEpochSecond() - currentTime.toEpochSecond();
-        long random = RandomUtil.randomLong(Math.abs(randomMax));
-        log.debug("{} latestTime: {}, randomMax: {}, random: {}", currentTime.format(DatePattern.NORM_DATETIME_FORMATTER), latestTime.format(DatePattern.NORM_DATETIME_FORMATTER), randomMax, random);
-
-        ZonedDateTime checkInTime = currentTime.plusSeconds(random);
-        taskScheduler.schedule(() -> checkinActuator.checkin(), checkInTime.toInstant());
-    }
+	// @Resource
+	// private CheckinActuator checkinActuator;
+	//
+	// @Resource
+	// private TaskScheduler taskScheduler;
+	//
+	// @SneakyThrows
+	// // @Scheduled(cron = "0 30 7 * * ?", zone = "Asia/Shanghai")
+	// public void checkin() {
+	//     ZonedDateTime currentTime = LocalDateTime.now().atZone(ZoneId.systemDefault());
+	//     ZonedDateTime latestTime = currentTime.plusHours(1);
+	//
+	//     long randomMax = latestTime.toEpochSecond() - currentTime.toEpochSecond();
+	//     long random = RandomUtil.randomLong(Math.abs(randomMax));
+	//     log.debug("{} latestTime: {}, randomMax: {}, random: {}", currentTime.format(DatePattern.NORM_DATETIME_FORMATTER), latestTime.format(DatePattern.NORM_DATETIME_FORMATTER), randomMax, random);
+	//
+	//     ZonedDateTime checkInTime = currentTime.plusSeconds(random);
+	//     taskScheduler.schedule(() -> checkinActuator.checkin(), checkInTime.toInstant());
+	// }
 
 }
